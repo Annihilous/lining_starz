395cdea854e91c1dd188e94cb45abf57c8e8da2a
Annihilous <benjaminjnorton@gmail.com>
2016-06-02 18:04:43 -0400

    Comment out after action to send email notice on new message creation.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ActiveRecord::Base
   validates_presence_of :name, :email, :description
 
-  after_create :email_message
+  # after_create :email_message
 
   def email_message
     UserMailer.new_message_mailer(self.id).deliver_later
